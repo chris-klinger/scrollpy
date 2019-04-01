f16aa396d5f255327f884c80116e5307a15eeb1f
Chris Klinger <cklinger@ualberta.ca>
2019-04-01 10:18:26 -0500

    Starting on sequences filtering.
    Currently committing old code that was intended to start the filtering
    process. For now, just filtering based on length, but eventually intend
    to filter based on other metrics as well.
    
    For this, need to keep in mind:
        1) Filtering code must be flexible to account for different methods
        2) Filtering code must be able to be run multiples times
        3) Ordering of filtering should be variable(?)

---
+++ tests/unit/filter/test_filter.py
@@ -0,0 +1,47 @@
+"""
+This module contains code for testing sequence filtering.
+"""
+
+
+import unittest
+
+
+from numpy import mean,median,std
+from numpy import append as np_append
+from numpy.random import seed,randn
+
+
+
+# Need to mock some data
+class MockSeq:
+    def __init__(self, value):
+        self.value = value
+
+    def __len__(self):
+        return self.value
+
+
+def _mock_data():
+    # seed generator to get same results
+    seed(1)
+    # generate 200 random values with mean 500 and stddev 10
+    lengths = 10 * randn(200) + 500  # This does not have natural outliers!
+    return lengths
+data = _mock_data()
+
+
+class TestFilterZScore(unittest.TestCase):
+    """Tests the base implementation with Z-scores"""
+
+    def setUp(self):
+        """Create data and
+
+
+if __name__ == '__main__':
+    lengths = _mock_data()
+    m,s = mean(lengths),std(lengths)
+    # Original data set does not contain any outliers by Z-score
+    # Add some extreme values
+    zlengths = np_append(lengths,[300, 400, 600, 700])
+    outlier_vals = [l for l in zlengths if abs(l-m)/s >= 3]
+    print(outlier_vals)

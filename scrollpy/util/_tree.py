f4c762cfca8707257b831ab410f2856ef16a1bde
Chris Klinger <cklinger@ualberta.ca>
2019-11-20 13:01:00 -0500

    Started working on placing sequences in trees.
    Implemented some helper functions adapted from previous stand-alone
    code and added relevant test code as well.
    
    Although most funtions appear to accept LeafSeq objects as input, the
    'is_group_outgroup' function requires TreeNode objects in order for the
    tree.get_common_ancestor() function to work.
    
    Moving forward, will need to determine whether this will cause problems
    with the interface(s) already set up in ScrollPy.

---
+++ scrollpy/util/_tree.py
@@ -0,0 +1,108 @@
+"""
+This module contains functions for working with ETE3 tree objects.
+"""
+
+
+def is_node_monophyletic(node_obj, leafseq_list):
+    """Checks node monophyly based on group.
+
+    Given a LeafSeq object, checks whether all its child nodes share
+    the same group attribute.
+
+    Args:
+        node_obj     (obj) : ETE3 TreeNode object.
+        leafseq_list (list): List of all associated LeafSeq objects.
+
+    Returns:
+        bool: True if node is monophyletic.
+
+    """
+    groups = get_node_groups(node_obj, leafseq_list)
+    if len(groups) == 1:
+        return True
+    return False
+
+
+def get_node_groups(node_obj, leafseq_list):
+    """Gets all group names under a given node.
+
+    Args:
+        node_obj     (obj) : ETE3 TreeNode object.
+        leafseq_list (list): List of all associated LeafSeq objects.
+
+    Returns:
+        set: A set of all group attributes of child LeafSeq objects.
+
+    """
+    groups = set()
+    for leaf in node_obj:
+        name = leaf.name
+        for leafseq in leafseq_list:
+             if leafseq.name == name:
+                 groups.add(leafseq._group)
+    return groups
+
+
+def last_monophyletic_ancestor(node_to_check, leafseq_list, previous_node=None):
+    """Finds the last ancestor of a node that is still monophyletic.
+
+    Recursively moves up nodes to find the last monophyletic ancestor.
+
+    Args:
+	node_to_check (obj)           : ETE3 TreeNode object.
+	leafseq_list  (list)          : List of all associated LeafSeq objects.
+	previous_node (obj, optional) : ETE3 TreeNode object. Defaults to None.
+
+    Returns:
+	obj: ETE3 TreeNode object representing most ancestral monophyletic node.
+	If starting node is not monophyletic, returns None instead.
+
+    """
+    if is_node_monophyletic(node_to_check, leafseq_list):
+        target = node_to_check.up  # Parent node
+        return last_monophyletic_ancestor(target, leafseq_list, node_to_check)
+    else:  # Basecase
+        return previous_node  # Last node that was monophyletic
+
+
+def is_group_outgroup(tree_obj, target_leaf, group_list):
+    """
+    For a given group of sequences, determines whether they can be used to
+    root the tree, in order to be able to look for the target_leaf.
+
+    Essentially, checks whether the common ancestor node of the group itself
+    includes the target_sequence, or, if the ancestor node is actually the
+    root, whether or not either of the sister node's children are the
+    sequence itself.
+
+    Could also check for monophyly?
+
+    Args:
+	tree_obj (obj)    : ETE3 Tree/TreeNode object.
+	target_leaf (str) : Name of leaf to place (same as node.name).
+	group_list (list) : List of TreeNode objects for a group.
+
+    Returns:
+	obj: ETE3 TreeNode object of a node to use for rerooting. If no
+	ancestral node can be used to reroot, returns False instead.
+
+    """
+    # Find common ancestor of all leaves
+    common_ancestor = tree_obj.get_common_ancestor(group_list)
+    # Check whether it is root - try again
+    if common_ancestor.is_root():
+        return False
+    else:  # Internal node
+        # Node can't already contain sequence of interest
+        for leaf in common_ancestor:
+            if leaf.name == target_leaf:
+                return False
+        # Also, target sequence can't be sister group
+        next_ancestor = common_ancestor.up
+        for node in next_ancestor.children:
+            if not node == common_ancestor:  # I.E., get sister
+                if node.is_leaf():
+                    if node.name == target_leaf:
+                        return False
+    return common_ancestor
+
---
+++ tests/unit/util/test_tree.py
@@ -0,0 +1,116 @@
+"""
+Tests /util/_tree
+"""
+
+import os
+import unittest
+
+
+from scrollpy.files import tree_file as tf
+from scrollpy.util._mapping import Mapping
+from scrollpy.util import _tree
+
+
+cur_dir = os.path.dirname(os.path.realpath(__file__)) # /files/
+# cleaner to use realpath due to relative path
+data_dir = os.path.realpath(os.path.join(cur_dir, '../../fixtures')) # /tests/
+
+
+class TestTreeModule(unittest.TestCase):
+    """Tests utility Tree functions with LeafSeq objects"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Creates the necessary mapping"""
+        tree_file = os.path.join(
+                data_dir,'Hsap_AP_EGADEZ_symmetric.tre')
+        map_file  = os.path.join(
+                data_dir,'Hsap_AP_EGADEZ_mapping.txt')
+
+        # Get tree object
+        cls.tree = tf.read_tree(tree_file,'newick')
+        # Get seq_dict
+        mapping = Mapping(
+                treefile=tree_file,
+                mapfile=map_file,
+                infmt='fasta',  # Not needed
+                treefmt='newick',
+                )
+        seq_dict = mapping()
+        # Flatten into a list
+        cls.leafseq_list = []
+        for _,leaves in seq_dict.items():
+            cls.leafseq_list.extend(leaves)
+
+
+    def test_get_node_groups(self):
+        """Tests that the function returns properly"""
+        target_nodes = [
+                'NP_003929.4','NP_031373.2','NP_055670.1',
+                ]
+        test_node = self.tree.get_common_ancestor(target_nodes)
+        # Check node groups
+        groups = _tree.get_node_groups(
+                test_node,
+                self.leafseq_list,
+                )
+        self.assertEqual(len(groups),1)
+        self.assertEqual(('group1' in groups),True)
+
+
+    def test_node_monophyly(self):
+        """Tests that the function returns properly"""
+        target_nodes = [
+                'NP_003929.4','NP_031373.2','NP_055670.1',
+                ]
+        test_node = self.tree.get_common_ancestor(target_nodes)
+        # Check node groups
+        monophyletic = _tree.is_node_monophyletic(
+                test_node,
+                self.leafseq_list,
+                )
+        self.assertEqual(monophyletic,True)
+
+
+    def test_last_monophyletic_ancestor(self):
+        """Tests that the node retrieved is monophyletic"""
+        expected_children = [
+                'NP_003929.4','NP_031373.2','NP_055670.1',
+                ]
+        test_node = self.tree&"NP_003929.4"
+        last_monophyletic = _tree.last_monophyletic_ancestor(
+                test_node,
+                self.leafseq_list,
+                )
+        actual_children = [node.name for node in last_monophyletic]
+        self.assertEqual(expected_children,actual_children)
+
+
+    def test_is_group_outgroup(self):
+        """Tests that re-rooting works correctly"""
+        # Target to actually search for
+        target_leaf = 'NP_031373.2'
+        # Nodes to at as the 'outgroup'
+        group_names = ['NP_001025178.1','NP_001229766.1']
+        # Test with both TreeNode and LeafSeq objects
+        group_nodes = [self.tree&name for name in group_names]
+        # leafseqs = [leafseq for leafseq in self.leafseq_list
+        #         if leafseq.name in group_names]
+        # # Actually run
+        node_root = _tree.is_group_outgroup(
+                self.tree,
+                target_leaf,
+                group_nodes,
+                )
+        # leafseq_root = _tree.is_group_outgroup(
+        #         self.tree,
+        #         target_leaf,
+        #         leafseqs,
+        #         )
+        # Get children
+        node_root_children = [leaf.name for leaf in node_root]
+        # leafseq_root_children = [leaf.name for leaf in leafseq_root]
+        # Now test equality
+        self.assertEqual(group_names,node_root_children)
+        # self.assertEqual(group_names,leafseq_root_children)
+
